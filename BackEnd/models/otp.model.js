0a0c77cb3b5ca81d872b604705a955f9cfce0043
sudeep9503 <sudeepswami9503@gmail.com>
2026-05-04 17:24:43 +0530

    Email verification using otp And referal removal

--- BackEnd/models/otp.model.js
+++ BackEnd/models/otp.model.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
 const otpSchema = new mongoose.Schema({
-  phone: { type: String, required: true, index: true },
-  otp: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 600 }, // auto-delete after 10 min
+  email: { type: String, index: true },
+  emailOtp: { type: String },
+  emailOtpExpiry: { type: Date },
+  emailVerified: { type: Boolean, default: false },
+  emailAttempts: { type: Number, default: 0 },
+  lastEmailRequest: { type: Date },
+  createdAt: { type: Date, default: Date.now, expires: 3600 },
 });
 
 export const OtpModel = mongoose.model("Otp", otpSchema);
